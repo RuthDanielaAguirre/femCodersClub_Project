72f7d37b42e50486b066a8330cca88444b29ea4a
karyoli.ie@gmail.com <148868767+nkaryoli@users.noreply.github.com>
2024-03-23 14:24:57 +0100

    #8 adding sponsor table component

---
+++ client/src/features/Admin/components/sponsor/SponsorContainer.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import SponsorTable from './SponsorTable'
+
+const SponsorContainer = () => {
+  return (
+    <div>
+        <SponsorTable/>
+    </div>
+  )
+}
+
+export default SponsorContainer
