dbe3147255e4a76917d393413af9fdb9a3c6b348
karyoli.ie@gmail.com <148868767+nkaryoli@users.noreply.github.com>
2024-03-25 15:53:30 +0100

    n trial

--- server/src/main.ts
+++ server/src/main.ts
@@ -6,7 +6,11 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({origin: true, methods: ["GET", "POST", "PUT", "DELETE"]});
+  app.enableCors({origin: "http://localhost:5173", 
+                  methods: 'GET, POST, PUT, DELETE', 
+                  allowedHeaders: 'Content-Type, Authorization',
+                  credentials: true, 
+                });
   app.use(session({
     secret: 'qwertyuiop',
 saveUninitialized: false,
