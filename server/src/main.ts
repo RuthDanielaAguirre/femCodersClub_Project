4963b002b53a236fd0261f17120da41ec1f5af30
karyoli.ie@gmail.com <148868767+nkaryoli@users.noreply.github.com>
2024-03-25 15:44:17 +0100

    trying to enable cors

--- server/src/main.ts
+++ server/src/main.ts
@@ -6,7 +6,7 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({origin:true});
+  app.enableCors({origin: true, methods: ["GET", "POST", "PUT", "DELETE"]});
   app.use(session({
     secret: 'qwertyuiop',
 saveUninitialized: false,
