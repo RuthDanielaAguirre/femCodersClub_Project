0e1f58ba82db8f19501040adc86d345538bdf249
91am <killaek.info@gmail.com>
2024-03-25 20:52:50 +0100

    #11 add icons and values cards to home page

--- client/src/features/Home/components/CardsEventsHome.tsx
+++ client/src/features/Home/components/CardsEventsHome.tsx
@@ -4,53 +4,49 @@ import { styles } from "../../../style";
 
 function CardsEventsHome() {
     return (
-        
         <div className="flex gap-16">
-        <div>
-            <Card
-                className="max-w-sm rounded-2xl"
-                renderImage={()=> <div className="bg-[url('../../../../img-events.png')] bg-cover bg-no-repeat h-64 m-6 flex items-end rounded-2xl "><h2 className="text-primary font-headerText font-bold ml-4 mb-4">Titulo: Phasellus aliquam luctus dignissim.</h2></div> }>
+            <div>
+                <Card
+                    className="max-w-sm rounded-2xl"
+                    renderImage={() => <div className="bg-[url('../../../../img-events.png')] bg-cover bg-no-repeat h-52 m-6 flex items-end rounded-2xl "><h2 className="text-primary font-headerText font-bold ml-4 mb-4">Titulo: Phasellus aliquam luctus dignissim.</h2></div>}>
 
-                <h5 className="text-sm font-bold tracking-tight text-gray-900 dark:text-white px-2">
-                    Fecha: Phasellus aliquam luctus dignissim.
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                </h5>
-                <p className="font-normal text-gray-700 dark:text-gray-400 px-2">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                </p>
-                <button className={`${styles.primaryBtn}`}>Ver más</button>
-            </Card>
-        </div>
-        <div>
-            <Card
-                className="max-w-sm rounded-2xl"
-                renderImage={()=> <div className="bg-[url('../../../../img-events.png')] bg-cover bg-no-repeat h-64 m-6 flex items-end rounded-2xl "><h2 className="text-primary font-headerText font-bold ml-4 mb-4">Titulo: Phasellus aliquam luctus dignissim.</h2></div> }>
+                    <h5 className="text-md text-contrast font-headerText font-bold px-2">
+                        Fecha: Phasellus aliquam luctus dignissim.
+                    </h5>
+                    <p className="text-md text-contrast font-bodyText px-2">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    </p>
+                    <button className={`${styles.primaryBtn}`}>Ver más</button>
+                </Card>
+            </div>
+            <div>
+                <Card
+                    className="max-w-sm rounded-2xl"
+                    renderImage={() => <div className="bg-[url('../../../../img-events.png')] bg-cover bg-no-repeat h-52 m-6 flex items-end rounded-2xl "><h2 className="text-primary font-headerText font-bold ml-4 mb-4">Titulo: Phasellus aliquam luctus dignissim.</h2></div>}>
 
-                <h5 className="text-sm font-bold tracking-tight text-gray-900 dark:text-white px-2">
-                    Fecha: Phasellus aliquam luctus dignissim.
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                </h5>
-                <p className="font-normal text-gray-700 dark:text-gray-400 px-2">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                </p>
-                <button className={`${styles.primaryBtn}`}>Ver más</button>
-            </Card>
-        </div>
-        <div>
-            <Card
-                className="max-w-sm rounded-2xl"
-                renderImage={()=> <div className="bg-[url('../../../../img-events.png')] bg-cover bg-no-repeat h-64 m-6 flex items-end rounded-2xl "><h2 className="text-primary font-headerText font-bold ml-4 mb-4">Titulo: Phasellus aliquam luctus dignissim.</h2></div> }>
+                    <h5 className="text-md text-contrast font-headerText font-bold px-2">
+                        Fecha: Phasellus aliquam luctus dignissim.
+                    </h5>
+                    <p className="text-md text-contrast font-bodyText px-2">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    </p>
+                    <button className={`${styles.primaryBtn}`}>Ver más</button>
+                </Card>
+            </div>
+            <div>
+                <Card
+                    className="max-w-sm rounded-2xl"
+                    renderImage={() => <div className="bg-[url('../../../../img-events.png')] bg-cover bg-no-repeat h-52 m-6 flex items-end rounded-2xl "><h2 className="text-primary font-headerText font-bold ml-4 mb-4">Titulo: Phasellus aliquam luctus dignissim.</h2></div>}>
 
-                <h5 className="text-sm font-bold tracking-tight text-gray-900 dark:text-white px-2">
-                    Fecha: Phasellus aliquam luctus dignissim.
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                </h5>
-                <p className="font-normal text-gray-700 dark:text-gray-400 px-2">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                </p>
-                <button className={`${styles.primaryBtn}`}>Ver más</button>
-            </Card>
-        </div>
+                    <h5 className="text-md text-contrast font-headerText font-bold px-2">
+                        Fecha: Phasellus aliquam luctus dignissim.
+                    </h5>
+                    <p className="text-md text-contrast font-bodyText px-2">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    </p>
+                    <button className={`${styles.primaryBtn}`}>Ver más</button>
+                </Card>
+            </div>
         </div>
     );
 }
--- client/src/features/Home/components/CarouselContactHome.tsx
+++ client/src/features/Home/components/CarouselContactHome.tsx
@@ -11,10 +11,10 @@ function CarouselContactHome() {
                         <h1 className="text-4xl font-semi-bold text-primary font-headerText mt-28">Si quieres recibir información haz click <Link to="/contacto" className="text-tertiary">aquí</Link></h1>
                 </div>
                 <div className="bg-[url('../../../../img-volunteer.png')] bg-cover bg-no-repeat h-screen w-full flex justify-center items-center">
-                        <h1 className="text-4xl font-semi-bold text-primary font-headerText mt-28">Si quieres recibir información haz click <Link to="/contacto" className="text-tertiary">aquí</Link></h1>
+                        <h1 className="text-4xl font-semi-bold text-primary font-headerText mt-28">Si quieres ser Voluntaria/o haz click <Link to="/contacto" className="text-tertiary">aquí</Link></h1>
                 </div>
                 <div className="bg-[url('../../../../img-sponsor.svg')] bg-cover bg-no-repeat h-screen w-full flex justify-center items-center">
-                        <h1 className="text-4xl font-semi-bold text-primary font-headerText mt-28">Si quieres recibir información haz click <Link to="/contacto" className="text-tertiary">aquí</Link></h1>
+                        <h1 className="text-4xl font-semi-bold text-primary font-headerText mt-28">Si quieres ser Sponsor haz click aquí <Link to="/contacto" className="text-tertiary">aquí</Link></h1>
                 </div>
             </Carousel>
         </div>
