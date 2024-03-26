7f10ead494adbb91675f0b09ee55e4dacb4e3b22
karyoli.ie@gmail.com <148868767+nkaryoli@users.noreply.github.com>
2024-03-26 13:10:12 +0100

    #16 deleting comments on home page login

--- client/src/features/LogIn/page/LogInPage.tsx
+++ client/src/features/LogIn/page/LogInPage.tsx
@@ -5,7 +5,7 @@ import bgLogin from '../../../../public/bgLogin.svg'
 const LogInPage = () => {
   return (
     <Layout>
-      <section className="flex justify-start items-center pl-[20%] h-screen w-full py-20 bg-[url('../../../public/bg-login.svg')] bg-no-repeat bg-cover bg-right relative ">
+      <section className="flex justify-start items-center pl-[20%] h-screen w-full py-20 relative ">
         <img src={bgLogin} className='absolute top-0 right-0 z-[-1]' alt="" />
         <div className='absolute left-0 top-0 w-[70%] h-[100%] bg-gradient-to-r from-primary to-transparent z-[1]'/>
         <div className='absolute left-0 top-0 w-[60%] h-[100%] bg-gradient-to-r from-primary to-transparent z-[1]'/>
