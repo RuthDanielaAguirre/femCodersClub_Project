3ea0580957cf5010d40c5aa973f67ece5b573ec2
karyoli.ie@gmail.com <148868767+nkaryoli@users.noreply.github.com>
2024-04-01 00:03:48 +0200

    #26 adding user in the return of login function in authService

--- client/src/features/User/components/UserProfile.tsx
+++ client/src/features/User/components/UserProfile.tsx
@@ -1,6 +1,10 @@
 import { styles } from "../../../style"
 
 const UserProfile = () => {
+    
+    const user = localStorage.getItem('user');
+    console.log(user);
+
     return (
         <div className="flex flex-col gap-4 lg:px-24 lg:py-10 bg-accent/15 rounded-[16px]">
             <div className="flex gap-4 text-contrast">
