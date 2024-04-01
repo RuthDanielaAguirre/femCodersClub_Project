9db80e013245cadc89add95590b70545bf2acd15
karyoli.ie@gmail.com <148868767+nkaryoli@users.noreply.github.com>
2024-04-01 13:36:55 +0200

    #26 profile section created, api call successfull

--- client/src/features/User/components/UserProfile.tsx
+++ client/src/features/User/components/UserProfile.tsx
@@ -1,31 +1,34 @@
 import { styles } from "../../../style"
 
 const UserProfile = () => {
-    
-    const user = localStorage.getItem('user');
+    const item = localStorage.getItem('user');
+    const user = JSON.parse(item as string);
+
     console.log(user);
+
+    const { name, lastName, email, gender, telephone} = user
 
     return (
         <div className="flex flex-col gap-4 lg:px-24 lg:py-10 bg-accent/15 rounded-[16px]">
             <div className="flex gap-4 text-contrast">
                 <span className="font-semibold">Nombre:</span>
-                <span>Maria</span>
+                <span>{name}</span>
             </div>
             <div className="flex gap-4 text-contrast">
                 <span className="font-semibold">Apellido:</span>
-                <span>Sanchez</span>
+                <span>{lastName}</span>
             </div>
             <div className="flex gap-4 text-contrast">
                 <span className="font-semibold">Género:</span>
-                <span>Femenino</span>
+                <span>{gender}</span>
             </div>
             <div className="flex gap-4 text-contrast">
                 <span className="font-semibold">Correo:</span>
-                <span>MariaSanchez@gmail.com</span>
+                <span>{email}</span>
             </div>
             <div className="flex gap-4 text-contrast">
                 <span className="font-semibold">Número de teléfono:</span>
-                <span>61258963</span>
+                <span>{telephone}</span>
             </div>
             <button className={`${styles.primaryBtn} w-[150px] flex items-center justify-center self-end`}>Editar</button>
         </div>
