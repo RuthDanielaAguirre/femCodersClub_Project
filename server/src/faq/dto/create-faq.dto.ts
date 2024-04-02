7bc1d15a0b7df5708280deacbb4982495afc5137
RuthDanielaAguirre <argdiee@icloud.com>
2024-04-02 11:36:01 +0200

    #19  Faq Section Admin and Dto

--- server/src/faq/dto/create-faq.dto.ts
+++ server/src/faq/dto/create-faq.dto.ts
@@ -1,4 +1,10 @@
 export class CreateFaqDto {
-        faqQuestion: string;
-        faqAnswer: string;
-}
+    faqQuestion: string = ''; 
+    faqAnswer: string = '';  
+  
+    constructor(faqQuestion: string, faqAnswer: string) {
+      this.faqQuestion = faqQuestion;
+      this.faqAnswer = faqAnswer;
+    }
+  }
+  
