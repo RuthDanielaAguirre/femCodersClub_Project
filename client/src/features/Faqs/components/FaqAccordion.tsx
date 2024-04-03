51b2fa4f2461173b859d01c3354abee094e47058
karyoli.ie@gmail.com <148868767+nkaryoli@users.noreply.github.com>
2024-04-03 17:13:49 +0200

    #34 Faqs page created with styles and bgImg added

--- client/src/features/Faqs/components/FaqAccordion.tsx
+++ client/src/features/Faqs/components/FaqAccordion.tsx
@@ -1,35 +1,37 @@
-
+import { useQuery } from '@tanstack/react-query';
+import { getFaq } from '../../../api/faqApi';
 import { IoIosArrowDown, IoIosArrowUp } from "react-icons/io";
 import { useState } from "react";
-import { Faq } from "../../../types/types";
 
-interface FAQProps {
-  faqs: Faq[];
-}
 
-function FaqAccordion({ faqs }: FAQProps) {
+function FaqAccordion() {
   const [activeIndex, setActiveIndex] = useState<number | null>(null);
 
   const toggleAccordion = (index: number) => {
     setActiveIndex((prevIndex) => (prevIndex === index ? null : index));
   };
+  
+  const { data } = useQuery({
+    queryKey: ["faq"],
+    queryFn: getFaq,
+  });
+
 
   return (
-    <div className="w-full">
-      {faqs.map((faq, index) => (
-        <div key={index} className="border-b border-gray-200">
+    <div className='z-10'>
+      {data ?.map((faq, index) => (
+        <div key={index} className="border-b border-4 border-transparent max-w-[800px]">
           <button
-            className={`flex justify-between items-center w-full p-4 focus:outline-none ${
-              activeIndex === index ? "bg-accent-80" : ""
-            }`}
+            className={`flex justify-between items-center w-full px-8 text-primary 
+              ${activeIndex === index ? "bg-gradient-to-r text-start from-accent/80 to-tertiary/80 font-bold text-xl py-6 rounded-t-xl" : "bg-accent/80 text-xl py-4 rounded-xl"}`}
             onClick={() => toggleAccordion(index)}
           >
-            <span>{faq.faqQuestion}</span>
+            {faq.faqQuestion}
             {activeIndex === index ? <IoIosArrowUp /> : <IoIosArrowDown />}
           </button>
           {activeIndex === index && (
-            <div className="p-4">
-              <p>{faq.faqAnswer}</p>
+            <div className="px-4 pb-4 bg-gradient-to-r from-accent/80 to-tertiary/80 rounded-b-xl">
+              <p className='bg-primary rounded-[24px] p-8 mx-4 mb-4'>{faq.faqAnswer}</p>
             </div>
           )}
         </div>
