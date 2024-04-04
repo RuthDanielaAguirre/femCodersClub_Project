e422041c4180dbf1dbdb5ed9c1477c0035660891
karyoli.ie@gmail.com <148868767+nkaryoli@users.noreply.github.com>
2024-04-04 19:56:59 +0200

    #38 integration of qrCode with embedded checkout. after user registers into and event, a qrCode is generated

--- client/src/features/Events/components/EmbeddedCheckout.tsx
+++ client/src/features/Events/components/EmbeddedCheckout.tsx
@@ -1,10 +1,31 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { createPortal } from "react-dom";
+import QrCodeModal from "./qrCode/QrCodeModal";
+import { useLocalStorage } from "../../../hooks/useLocalStorage";
+import QRCode from 'qrcode';
 
 const EmbeddedCheckout = ({ isVisible, onClose, eventId}: { isVisible: boolean, onClose: () => void, eventId:string}) => {
 
+    const [showQr, setShowQr] = useState<boolean>(false);
+    const [userName, setUserName] = useState('');
+    const [userLastName, setUserLastName] = useState('');
+    const [src, setSrc] = useState('');
+    const[currentUser, ] = useLocalStorage('user', '');
+    const { name, lastName } = currentUser;
+
+    const generate = () => {
+        const user = userName + ' ' + userLastName;
+        QRCode.toDataURL(`Asistente: ${user}, Id del Evento: #${eventId}`).then(setSrc);
+    }
+
+    const closeModal = () => onClose();
+
     const exampleCallback = () => { 
-        console.log('Order complete!'); 
+        setUserName(name);
+        setUserLastName(lastName);
+        generate();
+        closeModal();
+        setShowQr(true);
     };
     
     useEffect(() => {
@@ -24,8 +45,8 @@ const EmbeddedCheckout = ({ isVisible, onClose, eventId}: { isVisible: boolean, 
         <>
             {createPortal(
                 <div className='fixed inset-0 bg-black bg-opacity-25 backdrop-blur-sm flex justify-center items-center'>
-                    <div className="relative rounded-2xl overflow-hidden w-[1000px]">
-                        <button className="absolute right-3 top-2 text-primary text-2xl place-self-end" onClick={()=> onClose()}>(x)</button>
+                    <div className="relative rounded-2xl overflow-hidden w-[960px]">
+                        <button className="absolute right-3 top-2 text-primary text-2xl place-self-end" onClick={closeModal}>(x)</button>
                         <div id={`eventbrite-widget-container-${eventId}`} className="w-full">
                         </div>
                     </div>
@@ -36,6 +57,7 @@ const EmbeddedCheckout = ({ isVisible, onClose, eventId}: { isVisible: boolean, 
         :
         <></>
         }
+        <QrCodeModal showQr={showQr} onClose={() => setShowQr(false)} qrCode={src}/>
         </>
     )
 }
