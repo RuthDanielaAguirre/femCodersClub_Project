251a2318e831c38846c950ce2f6c7c6e013476cb
Belén <alonsoperalbelen@gmail.com>
2024-04-04 12:16:56 +0200

    envirorment variables ddbb

--- server/src/main.ts
+++ server/src/main.ts
@@ -7,17 +7,6 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  app.use(session({
-    secret: 'qwertyuiop',
-saveUninitialized: false,
-resave: false,
-cookie: {
-  maxAge: 7200000,
-},
-  }),
-  );
-  app.use(passport.initialize());
-  app.use(passport.session());
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(process.env.PORT);
 }
