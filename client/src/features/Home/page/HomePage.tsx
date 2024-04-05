8df0c695a3d7376b0f0867647f836bbb800a052c
91am <killaek.info@gmail.com>
2024-04-05 20:05:11 +0200

    #42 fix hero section and add responsive

--- client/src/features/Home/page/HomePage.tsx
+++ client/src/features/Home/page/HomePage.tsx
@@ -23,28 +23,44 @@ const HomePage = () => {
   return (
     <Layout>
 
-     
-<section className="flex flex text-left bg-center" style={{ backgroundImage: `url(${heroImg2})`, backgroundSize: 'cover', backgroundRepeat: "no-repeat", height: 'vh' }}>
-  <div className="flex flex-col items-start justify-center px-16">
-    <h1 className={`${styles.heading1} font-headerTex w-16 leading-none`}>FemCoders Club</h1>
-    <br />
-    <h2 className="text-2xl font-bold text-secondary font-headerText"><span className="text-tertiary">Juntas</span>  potenciamos el <br />crecimiento y liderazgo de las <br /> mujeres tech.</h2>
-    <div className='py-2 flex gap-4 mt-4'>
-      <Link to="/contacto"><button className={`${styles.primaryBtn} w-[180px] text-bodyText`}>Únete al club</button></Link>
-      <Link to="/eventos"><button className={`${styles.primaryBtn} w-[180px] text-bodyText `}>Ver eventos</button></Link>
-    </div>
-  </div>
-</section>
+      <section className='lg:h-screen h-full w-full flex flex-col lg:flex-row'>
 
 
-
-      <section className='h-screen w-full flex justify-center items-center mt-24 mb-8'>
-        <div className='flex flex-col items-center'>
-          <div className='flex'>
-          <h2 className={`${styles.heading2} font-headerText mb-16`}>¡Próximos eventos!</h2>
-          <Link to="/eventos"><p className={`${styles.text}text-xl font-bold text-secondary font-headerText py-2 absolute ml-20 hover:border-b-2 border-tertiary`}>Ver todos los eventos</p></Link>
+        <div className="h-full flex flex-col justify-center items-center text-center px-2 order-2 lg:order-1 lg:px-16 lg:text-left ">
+          <h1 className={`${styles.heading1} px-4 py-4 font-headerText leading-none text-center lg:text-left`}>FemCoders Club</h1>
+          <br />
+          <h2 className="text-2xl font-bold text-secondary font-headerText px-4"><span className="text-tertiary">Juntas</span>  potenciamos el crecimiento y liderazgo de las mujeres tech.</h2>
+          <div className='py-2 px-4 flex flex-col lg:flex-row gap-4 mt-4'>
+            <Link to="/contacto"><button className={`${styles.primaryBtn} w-[180px] text-bodyText`}>Únete al club</button></Link>
+            <Link to="/eventos"><button className={`${styles.primaryBtn} w-[180px] text-bodyText `}>Ver eventos</button></Link>
           </div>
-          <CardUpcomingEvent />
+        </div>
+        <img src={heroImg2} className="w-full order-1" alt="" />
+
+
+      </section>
+
+
+      <section className='h-full w-full flex justify-center items-center px-4 my-16'>
+        <div className='flex flex-col items-center gap-y-8'>
+          <div className='flex'>
+            <h2 className={`${styles.heading2} font-headerText my-8 text-center leading-none`}>¡Próximos eventos!</h2>
+            <Link to="/eventos"><p className={`${styles.text}text-xl font-bold text-secondary font-headerText py-2 absolute ml-20 hover:border-b-2 border-tertiary`}>Ver todos los eventos</p></Link>
+          </div>
+          {!isLoadingUpcomingEvents && upcomingEventsData.events.map((event: { start: { local: string | number | Date; }; name: { text: string; }; logo: { original: { url: string; }; }; venue: { address: { localized_address_display: string; }; }; description: { text: string; }; id: string }) => {
+
+            const date = new Date(event?.start?.local)
+            const formateDate = date.toLocaleDateString("es-ES", { weekday: "long", month: "long", day: "numeric", hour: "numeric", minute: "numeric", hour12: true })
+
+            return <CardUpcomingEvent
+              title={event.name.text}
+              image={event?.logo?.original?.url}
+              date={formateDate}
+              location={event?.venue?.address?.localized_address_display}
+              description={event.description.text}
+              eventId={event.id}
+            />
+          })}
         </div>
       </section>
 
@@ -54,7 +70,7 @@ const HomePage = () => {
           <h3 className={`${styles.heading3} font-headerText py-2 leading-tight`}>En FemCoders Club </h3>
           <p className="text-2xl text-primary font-bodyText pb-12">Somos un vibrante equipo de mujeres apasionadas por la tecnologia y el desarrollo web</p>
           <CarouselValues />
-          <p className={`${styles.heading5} text-primary font-base font-headerText pt-8 mx-8`}>FRASE CÉLEBRE DE UNA MUJER IMPORTANTE EN LA TECNOLOGÍA</p>
+          <p className={`${styles.heading5} text-primary font-base font-headerText pt-8 mx-8`}>"La tecnología no tiene género, y cada mujer tiene el potencial de ser una líder en este campo si se le brindan las oportunidades y el apoyo adecuados." - Mariana Costa (Perú)</p>
           <Link to="/sobrenosotras"><p className='text-2xl text-primary font-bodyText flex justify-center items-center hover:border-b-2 border-primary'>Ver más</p></Link>
         </div>
       </section>
@@ -63,7 +79,7 @@ const HomePage = () => {
         <CarouselContactHome />
       </section>
 
-      
+
     </Layout>
   )
 }
