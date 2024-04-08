3687f1d3039d243820baaa05f539cee02def761c
karyoli.ie@gmail.com <148868767+nkaryoli@users.noreply.github.com>
2024-04-08 19:21:27 +0200

    #35 adjusting styles of menber cards.

--- client/src/features/Team/components/CardTeamMember.tsx
+++ client/src/features/Team/components/CardTeamMember.tsx
@@ -28,49 +28,40 @@ const CardTeamMember = () => {
   return (
     <div className="flex flex-wrap lg:gap-24 md:gap-16 gap-8 justify-center items-start max-w-[1200px] p-5">
       {data?.map((member) => (
-        <div key={member.idMember} className={`${styles.cards} relative bg-gradient-to-br from-accent/40 to-tertiary/40 text p-4 w-64 h-72`}>
-          <div className="flex flex-col h-full ">
-            <div>
-            <div className="flex flex-col items-center">
-              <img src={member.memberImage} alt="member image" className="w-24 h-24 rounded-full bg-primary/80 h-full self-center" />
+        <div key={member.idMember} className={`${styles.cards} flex flex-col justify-between relative bg-gradient-to-br from-accent/40 to-tertiary/40 text  p-6 w-64 h-72`}>
+            <div className="flex flex-col gap-4">
+              <div className="flex justify-center">
+                <img src={member.memberImage} alt="member image" className="w-24 h-24 rounded-full bg-primary/80 self-center" />
               </div>
-              <h5 className={`${styles.text} font-bold flex items-center gap-2`}><FaUser className="text-secondary text-l" />{member.memberName} {member.memberLastName}</h5>
-              <p className={`${styles.text} flex items-center gap-2`}><HiUserGroup className="text-secondary text-l" />{member.memberRole}</p>
-               <p className={`${styles.text} flex items-center gap-2`}><span>< FaLinkedin className="text-l text-secondary" /></span>{member.memberLinkedin.slice(0,20)}...</p>
-            </div>
-            <div className="flex flex-grow flex-col justify-between">
-              <p className={`${styles.text} flex items-center gap-2 mb-4`}>
-                <PiFileMagnifyingGlassBold className="text-secondary text-l" />
-               
-
-                <span>{member.memberDescription.slice(0, 20)}...</span>
-              </p>
-              <div className="flex justify-end">
-                <button className="font-semibold text-secondary" onClick={() => handleClick(member)}>Ver más</button>
+              <div>
+                <h5 className={`${styles.text} font-bold flex items-center gap-2`}><FaUser className="text-secondary text-l" />{member.memberName} {member.memberLastName}</h5>
+                <p className={`${styles.text} flex items-center gap-2`}><HiUserGroup className="text-secondary text-l" />{member.memberRole}</p>
+                <p className={`${styles.text} flex items-center gap-2`}><span>< FaLinkedin className="text-l text-secondary" /></span>{member.memberLinkedin.slice(0,20)}...</p>
+                <p className={`${styles.text} flex items-center gap-2 mb-4`}>
+                  <PiFileMagnifyingGlassBold className="text-secondary text-l" />
+                  <span>{member.memberDescription.slice(0, 20)}...</span>
+                </p>
               </div>
             </div>
-          </div>
+            <button className="font-semibold text-secondary flex self-end" onClick={() => handleClick(member)}>Ver más</button>
         </div>
       ))}
       {selectedMember && (
         <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50 ">
           <div className=" bg-white bg-gradient-to-br from-primary to-accent/40 p-8 rounded-[30px] overflow-y-auto lg:max-w-[40vw] max-h-[100vh]  md:max-w-[70vw] max-h-[100vh] sm: max-w-[80vw] max-h-[100vh] ">
             <div className="flex flex-col items-center">
-            <img src={selectedMember.memberImage} alt="member image" className="w-44 h-44 rounded-full bg-primary/80 h-full self-center mb-4" />
-            <h5 className={`${styles.heading5} text-secondary font-bold flex items-center gap-2`}>{selectedMember.memberName} {selectedMember.memberLastName}</h5>  
-            <p className={`${styles.text} flex items-center pt-2 gap-2`}>| {selectedMember.memberRole} |</p> 
-            <p className={`${styles.text} flex items-center mt-2 text-center text-secondary`}>{selectedMember.memberLinkedin}</p>
-           </div> 
-           <p className={`${styles.text} flex text-center items-start gap-2 pb-4 pt-4`}>
+              <img src={selectedMember.memberImage} alt="member image" className="w-44 h-44 rounded-full bg-primary/80 h-full self-center mb-4" />
+              <h5 className={`${styles.heading5} text-secondary font-bold flex items-center gap-2`}>{selectedMember.memberName} {selectedMember.memberLastName}</h5>  
+              <p className={`${styles.text} flex items-center pt-2 gap-2`}>| {selectedMember.memberRole} |</p> 
+              <a href={selectedMember.memberLinkedin} className={`${styles.text} flex items-center mt-2 text-center text-secondary`}>{selectedMember.memberLinkedin}</a>
+            </div> 
+            <p className={`${styles.text} flex text-center items-start gap-2 pb-4 pt-4`}>
               <span>{selectedMember.memberDescription}</span>
             </p> 
-           
-
             <div className="flex flex-col items-center">
-             <button className={`${styles.primaryBtn} w-[80px]`} onClick={handleCloseModal} >Cerrar</button>
+              <button className={`${styles.primaryBtn} w-[80px]`} onClick={handleCloseModal} >Cerrar</button>
+            </div>
           </div>
-          </div>
-        
         </div>
       )}
     </div>
