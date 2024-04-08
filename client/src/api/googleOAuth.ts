c1fc0bedf65ff56dc5cb4d29224e5d5bcfb3fad4
karyoli.ie@gmail.com <148868767+nkaryoli@users.noreply.github.com>
2024-04-08 14:56:22 +0200

    #45 google login client side complete

--- client/src/api/googleOAuth.ts
+++ client/src/api/googleOAuth.ts
@@ -1,9 +1,10 @@
 import axios from "axios";
 import {  User } from "../types/types";
 
-const url = 'https://femcodersclub-project.onrender.com/google-authentication';
-export const googleAuth = async (tokenData:unknown): Promise<User> => {
-        const result = await axios.post(url, {tokenData});
-        console.log(result.data);
+const url = 'http://localhost:3000/google-authentication';
+export const googleAuth = async (token:string|undefined): Promise<User> => {
+        console.log(token)
+        const result = await axios.post(url, {token});
+        // console.log(result.data);
         return result.data;
 }
--- client/src/components/GoogleAuthButton.tsx
+++ client/src/components/GoogleAuthButton.tsx
@@ -1,50 +1,35 @@
-import { useGoogleLogin } from '@react-oauth/google';
+import { CredentialResponse, GoogleLogin } from '@react-oauth/google';
+import axios from 'axios';
+import { useLocalStorage } from '../hooks/useLocalStorage';
+import { useNavigate } from 'react-router-dom';
 import { styles } from '../style';
-// import axios from 'axios';
-// import { useLocalStorage } from '../hooks/useLocalStorage';
-// import { useNavigate } from 'react-router-dom';
-import { googleAuth } from '../api/googleOAuth';
-import iconGoogle from '../../public/iconGoogle.svg'
+
 
 const GoogleAuthButton = () => {
-    // const[, setUser] = useLocalStorage('user', '');
-    // const navigate = useNavigate();
-    
-    const login = useGoogleLogin({
-        onSuccess: async (response) => {
-            try {
-                // const res = await axios.get("https://www.googleapis.com/oauth2/v3/userinfo?access_token=" + response.access_token);
-                // console.log(res);
-                
-                // const authUser = {
-                //     name: res.data.given_name,
-                //     lastName: res.data.family_name,
-                //     gender: '',
-                //     email: res.data.email,
-                //     telephone:'',
-                // }
-                // setUser(authUser);
-                
-                const accessToken = response.access_token;
+    const[, setUser] = useLocalStorage('user', '');
+    const navigate = useNavigate();
 
-                const tokenData = JSON.stringify({token: accessToken})
-                const authUser = await googleAuth(tokenData)
-                // console.log(authUser);
-                // setUser(authUser);
-
-                // navigate('/')
-            } catch (err) {
-                console.log(err);
-            }
-        },
-    });
+    const login = async (credentialResponse: CredentialResponse) => {
+        try{
+            const result = await axios.post('https://femcodersclub-project.onrender.com/google-authentication', {token:credentialResponse.credential});
+            const googleUser = result.data;
+            console.log(googleUser);
+            setUser(googleUser);
+            navigate('/')
+        }catch(err){
+            console.log(err, "error in conection with backend")
+        }
+    }
 
     return (
-        <div className=' '>
-            <button onClick={() => login()} className={`${styles.secondaryBtn}`}>
-                <img src={iconGoogle} alt="icon-google" className="w-5" />
-                Continuar con Google
-            </button>
+        <div className={`${styles.secondaryBtn}`}>
+            <GoogleLogin
+                onSuccess={credentialResponse => {login(credentialResponse)}}
+
+                onError={() => {
+                    console.log('Login Failed');
+                }}
+                />
         </div>
     )
 }
