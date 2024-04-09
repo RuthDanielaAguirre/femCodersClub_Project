32309894dda288755d0d384a741be8eb39989c93
karyoli.ie@gmail.com <148868767+nkaryoli@users.noreply.github.com>
2024-04-09 16:45:14 +0200

    #49 api url added to .env. login works

--- client/src/api/authApi.ts
+++ client/src/api/authApi.ts
@@ -1,10 +1,12 @@
 import { User } from "../types/types"
 import axios from 'axios';
- const url= 'https://femcodersclub-project.onrender.com/auth/login';
- //const url= 'https://backend-marketplace-1l2b.onrender.com/auth/login';
+
+const url = `${import.meta.env.VITE_API_URL}/auth/login`;
+
 const authApi = async (email: string, password: string): Promise<User> => {
     const result = await axios.post(url, {userEmail: email, userPassword: password});
   return result.data;
 }
 
-export default authApi
+export default authApi;
+
