4613ca570bd2683ac19d34143fe505fb6dbca415
karyoli.ie@gmail.com <148868767+nkaryoli@users.noreply.github.com>
2024-04-09 17:19:39 +0200

    #49 url updated with .env at sponsorApi

--- client/src/api/sponsorApi.ts
+++ client/src/api/sponsorApi.ts
@@ -1,24 +1,23 @@
 import axios from "axios";
 import { Sponsor } from "../types/types";
 
-const url = 'https://femcodersclub-project.onrender.com/admin';
 export const addSponsor = async (sponsorsName:string, sponsorsCompany:string, sponsorsEmail:string, sponsorsTelephone:string): Promise<Sponsor> => {
-        const result = await axios.post(url, { sponsorsName:sponsorsName, sponsorsCompany:sponsorsCompany, sponsorsEmail:sponsorsEmail, sponsorsTelephone:sponsorsTelephone});
+        const result = await axios.post(`${import.meta.env.VITE_API_URL}/admin`, { sponsorsName:sponsorsName, sponsorsCompany:sponsorsCompany, sponsorsEmail:sponsorsEmail, sponsorsTelephone:sponsorsTelephone});
         return result.data;
 };
 
 export const getSponsors = async (): Promise<Sponsor[]> => {
-        const response = await axios.get(url);
+        const response = await axios.get(`${import.meta.env.VITE_API_URL}/admin`);
         return response.data;
 };
 
 export const updateSponsor = async (idPotential_Sponsors: string, updatedSponsor:{sponsorsName:string, sponsorsCompany:string, sponsorsEmail:string, sponsorsTelephone:string}): Promise<Sponsor> => {
-        const response = await axios.put(`${url}/${idPotential_Sponsors}`, updatedSponsor);
+        const response = await axios.put(`${import.meta.env.VITE_API_URL}/admin/${idPotential_Sponsors}`, updatedSponsor);
         return response.data;
 };
 
 export const deleteSponsor = async (idPotential_Sponsors: string): Promise<Sponsor> => {
-        const response = await axios.delete(`${url}/${idPotential_Sponsors}`);
+        const response = await axios.delete(`${import.meta.env.VITE_API_URL}/admin/${idPotential_Sponsors}`);
         return response.data;
 };
 
