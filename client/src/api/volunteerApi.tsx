850c92886a8c1ab48206446ec8aa79ffc46d1c15
karyoli.ie@gmail.com <148868767+nkaryoli@users.noreply.github.com>
2024-04-09 17:24:26 +0200

    #49 url updated with .env at vlunteerApi

--- client/src/api/volunteerApi.tsx
+++ client/src/api/volunteerApi.tsx
@@ -1,24 +1,22 @@
 import axios from "axios";
 import { Volunteer } from "../types/types";
 
-const API_URL = 'https://femcodersclub-project.onrender.com/volunteer'
 export const getVolunteers = async (): Promise<Volunteer[]> => {
-    const response = await axios.get(API_URL);
+    const response = await axios.get(`${import.meta.env.VITE_API_URL}/volunteer`);
     return response.data;
 };
 
-
 export const addVolunteer = async (volunteerName:string, volunteerLastName:string, volunteerEmail:string, volunteerGender:string): Promise<Volunteer> => {
-        const response = await axios.post(API_URL, { volunteerName:volunteerName, volunteerLastName:volunteerLastName, volunteerEmail:volunteerEmail, volunteerGender:volunteerGender});
+        const response = await axios.post(`${import.meta.env.VITE_API_URL}/volunteer`, { volunteerName:volunteerName, volunteerLastName:volunteerLastName, volunteerEmail:volunteerEmail, volunteerGender:volunteerGender});
         return response.data;
 }
 
 export const updateVolunteer = async (idVolunteer: string, updatedVolunteer:{volunteerName:string, volunteerLastName:string, volunteerEmail:string, volunteerGender:string}): Promise<Volunteer> => {
-    const response = await axios.put(`${API_URL}/${idVolunteer}`, updatedVolunteer);
+    const response = await axios.put(`${import.meta.env.VITE_API_URL}/volunteer/${idVolunteer}`, updatedVolunteer);
     return response.data;
 };
 
 export const deleteVolunteer = async  (idVolunteer: string): Promise<Volunteer> =>{
-    const response= await axios.delete(`${API_URL}/${idVolunteer}`);
+    const response= await axios.delete(`${import.meta.env.VITE_API_URL}/volunteer/${idVolunteer}`);
     return response.data;
 }
