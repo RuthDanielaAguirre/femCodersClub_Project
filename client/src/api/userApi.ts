7b30bab51faea4125e153e53f3f43aed9076eb3f
karyoli.ie@gmail.com <148868767+nkaryoli@users.noreply.github.com>
2024-04-09 17:21:15 +0200

    #49 url updated with .env at userApi

--- client/src/api/userApi.ts
+++ client/src/api/userApi.ts
@@ -1,8 +1,7 @@
 import axios from "axios";
 import { User } from "../types/types";
 
-const url = 'https://femcodersclub-project.onrender.com/user';
 export const updateUser = async (idUser: string, updatedUser:{userName:string, userLastName:string, userEmail:string, userGender:string, userTelephone:string}): Promise<User> => {
-    const response = await axios.put(`${url}/${idUser}`, updatedUser);
+    const response = await axios.put(`${import.meta.env.VITE_API_URL}/user/${idUser}`, updatedUser);
     return response.data;
 };
